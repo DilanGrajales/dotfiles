c8965ef5b76cf8dd0651895e1873305d585ea98f
Dilan Grajales <dilangrajales1128@gmail.com>
2025-08-10 23:07:09 -0600

    Adding more scripts

--- .screenlayout/single_external_monitor.sh
+++ .screenlayout/single_external_monitor.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
-xrandr --output eDP-1 --off --output DP-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-2 --off
+
+# Comprueba si el monitor DP-1 está conectado
+if xrandr --query | grep -q "DP-1 connected"; then
+    # Si está conectado, apaga la pantalla de la laptop y usa solo el monitor externo.
+    xrandr --output eDP-1 --off \
+           --output DP-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal \
+           --output HDMI-1 --off \
+           --output DP-2 --off
+else
+    # Si no está conectado, usa solo la pantalla de la laptop y apaga las demás.
+    xrandr --output eDP-1 --primary --auto \
+           --output DP-1 --off \
+           --output HDMI-1 --off \
+           --output DP-2 --off
+fi
